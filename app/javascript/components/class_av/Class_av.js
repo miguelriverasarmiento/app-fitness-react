d0b430a44a01cef72f90d117df556aead830caa8
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2022-03-23 11:05:04 -0500

    components working fine

--- app/javascript/components/class_av/Class_av.js
+++ app/javascript/components/class_av/Class_av.js
@@ -3,67 +3,74 @@ import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 function Class_av() {
+
 	const [ class_avs, setClass_avs ] = useState([])
-	const [class_av, setClass_av] = useState({name: '', date: '', quotas: ''});
+	const [ class_av, setClass_av ] = useState({name: '', date: '', quotas: ''});
 	
 	useEffect(() => {
 
-		axios.get('/api/v1/class_avs')
-		.then( res => {
-			setClass_avs(res.data)
-		})
-		.catch( res => console.log(res) )
+			axios.get('/api/v1/class_avs')
+			.then( res => {
+				setClass_avs(res.data)
+			})
+			.catch( res => console.log(res) )
 	}, [class_avs.length])
 
-	const csrfToken = document.querySelector('[name="csrf-token"]').content;
-        axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+		const csrfToken = document.querySelector('[name="csrf-token"]').content;
+	        axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
 
-    let navigate = useNavigate();
+	    let navigate = useNavigate();
 
-	function ClassBooking(class_av, e) { 
+	function ClassBooking(class_av, e) {
 
 		let quotas = class_av.quotas
-		if(quotas>=1){
-			let quo = quotas -= 1;
-			let id = class_av.id;
-			
-		axios.put(`/api/v1/class_avs/${id}`, { name: class_av.name, date: class_av.date, quotas: quo })
-			.then((res) => {
-				console.log(res.data);
-			})
-			.catch( res => console.log(res));
 
+			if(quotas>=1){
+				
+				let quo = quotas -= 1;
+				let id = class_av.id;
+				const updateClas = { name: class_av.name, date: class_av.date, quotas: quo }
+				
+				axios.put(`/api/v1/class_avs/${id}`, updateClas)
+				.then((res) => {
+					console.log(res.data)
+				})
+				.catch( res => console.log(res))
 			}else{
-			window.alert('Los cupos para esta clase se han acabado');
-			return
-			}
-		
-		axios.post('/api/v1/bookings', class_av)
-			.then((response) => {
-				console.log(response.data);
-			}).catch((error) => {
-	          	console.log(error);
-			});
-			navigate('/bookings');
-	}
-	
+			    window.alert('Los cupos para esta clase se han acabado');
+				return;
+				}
+			
+			axios.post('/api/v1/bookings', class_av)
+				.then((response) => {
+					console.log(response.data);
+				}).catch( res => console.log(res))
+
+		navigate('/bookings');
+
+		}
+
 	function ClassDelete(id, e) {
 		axios.delete(`/api/v1/class_avs/${id}`)
 		.then((response) => {
                const class_avsList = class_avs.filter(class_av => class_av.id !== id);
                setClass_avs(class_avsList);
-               
+
            }).catch((error) => {
                console.log(error.response.data)
            });
-	}
+		}
+
+	function sortClas(){
+    	return class_avs.sort((a, b) => (a.name > b.name ? 1 : a.name < b.name ? -1 : 0))
+    	}
 
 	return(
 
 		<React.Fragment>
 			<div className="classPage">
 				<h1 className="classTitle">Clases Disponibles</h1>
-				
+
 				<table>
 					<thead>
 						<tr>
@@ -75,7 +82,7 @@ function Class_av() {
 						</tr>
 					</thead>
 					<tbody>
-						{class_avs.map(class_av => (
+						{sortClas().map(class_av => (
 							<tr key={class_av.id} className="fileFill">
 								<td>{class_av.name}</td>
 								<td>{class_av.date}</td>
@@ -95,7 +102,7 @@ function Class_av() {
 
 		</React.Fragment>
 
-		);	
+		);
 
 	}
 
